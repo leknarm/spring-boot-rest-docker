123444575655889c141dcd75773bee75d8742e8a
Tantai Thanakanok <leknarm@gmail.com>
2017-03-16 16:36:46 +0700

    init project with https://start.spring.io/

---
+++ src/main/java/com/leknarm/SpringBootRestDockerApplication.java
@@ -0,0 +1,12 @@
+package com.leknarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestDockerApplication.class, args);
+	}
+}
